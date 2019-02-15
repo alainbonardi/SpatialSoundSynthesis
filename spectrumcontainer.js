6483990244341ef3d015de80003ce9f59bc037b5
Alain Bonardi <alain.bonardi@gmail.com>
2019-02-15 22:12:03 +0100

    started spectrum class

--- spectrumcontainer.js
+++ spectrumcontainer.js
@@ -30,10 +30,23 @@ partial.prototype.freqmult = function(factor) {
 	this.freq = this.freq * factor;
 }
 //----------------------------------------------------------------------------------------//
-//This method prints in the console the content of the class
+//This method prints in the console the content of the partial class
 //----------------------------------------------------------------------------------------//
 partial.prototype.print = function() {
 	post("frequency=", this.freq, " amplitude=", this.amp, " duplicated=", this.duplicated, "\n");
+}
+
+//----------------------------------------------------------------------------------------//
+//To test partial class
+//----------------------------------------------------------------------------------------//
+function partial_test() {
+	p1 = new partial(440, 0.3);
+	post("p1_____", "\n");
+	p1.print();
+	p1.freqshift(100);
+	p1.print();
+	p1.freqmult(3);
+	p1.print();
 }
 //----------------------------------------------------------------------------------------//
 //----------------------------------------------------------------------------------------//
@@ -44,6 +57,23 @@ function spectrum(N) {
 	this.partialNumber = N;
 	this.partialList = new Array(N);
 	for (i = 0; i < N; i++) {
-		this.partialList[i] = new partial(i*100, 0);
+		this.partialList[i] = new partial(i*50, 0);
 	}
 }
+
+//----------------------------------------------------------------------------------------//
+//This method prints in the console the content of the spectrum class
+//----------------------------------------------------------------------------------------//
+spectrum.prototype.print = function() {
+	var i, n;
+	n = this.partialNumber;
+	for (i = 0; i < n; i++) {
+		this.partialList[i].print();
+	}
+}
+
+function spectrum_test() {
+	s1 = new spectrum(100);
+	s1.print();
+}
+
